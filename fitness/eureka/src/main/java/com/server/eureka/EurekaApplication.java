68e08b7976689dcf88c50e97a9ff642470055d14
logesh <logesh.7871ll@gmail.com>
2026-08-01 10:38:29 +0530

    adding eureka setup

--- fitness/eureka/src/main/java/com/server/eureka/EurekaApplication.java
+++ fitness/eureka/src/main/java/com/server/eureka/EurekaApplication.java
@@ -2,9 +2,10 @@ package com.server.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@Enable
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
